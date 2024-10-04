6275880431a830da21982d78da31b668c6b6e36e
remon351 <remon7105@gmail.com>
2024-10-04 20:58:07 +0300

    Add project files.

---
+++ Hospital/Controllers/bookappointmentController.cs
@@ -0,0 +1,20 @@
+﻿using Hospital.data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital.Controllers
+{
+    public class bookappointmentController : Controller
+    {
+        ApplicationDbContext dbContext = new ApplicationDbContext();
+        public IActionResult Index()
+        {
+            var doctors = dbContext.doctors.ToList();
+            return View(model:doctors);
+        }
+        public IActionResult details(int id)
+        {
+            var doctorname = dbContext.doctors.Find(id);
+            return View(doctorname);
+        }
+    }
+}
---
+++ Hospital/data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Hospital.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital.data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Doctor> doctors { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build()
+                .GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(builder);
+        }
+              protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Doctor>().HasData(
+                new Doctor { Id = 1, Name = "Dr. John Smith", Specialization = "Cardiology", Img = "doctor1.jpg" },
+                new Doctor { Id = 2, Name = "Dr. Sarah Johnson", Specialization = "Pediatrics", Img = "doctor2.jpg" },
+                new Doctor { Id = 3, Name = "Dr. Emily Davis", Specialization = "Dermatology", Img = "doctor4.jpg" },
+                new Doctor { Id = 4, Name = "Dr. Michael Lee", Specialization = "Orthopedics", Img = "doctor3.jpg" },
+                new Doctor { Id = 5, Name = "Dr. William Clark", Specialization = "Neurology", Img = "doctor5.jpg" }
+            );
+        }
+    }
+}
